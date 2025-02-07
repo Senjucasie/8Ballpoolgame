02ae03c5e3a93515d66c9707843028aa4086ca57
Senjucasie <paulkpraveen@gmail.com>
2025-02-07 10:56:29 +0530

    Added logic for horizontal and verical mouse movement to handle the rotation of the cue stick

--- Assets/Scripts/CueStick.cs
+++ Assets/Scripts/CueStick.cs
@@ -5,16 +5,58 @@ using UnityEngine;
 public class CueStick : MonoBehaviour
 {
     private Transform _transform;
-
+    private Vector3 _prevPosition;
+    private Vector3 _differnce;
+    private Vector2 _speed = Vector2.zero;
     private void Awake()
     {
         _transform =transform.parent;  
     }
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+       
+        if(Input.GetMouseButtonDown(0))
         {
-            _transform.Rotate(Vector3.up,Space.World);
+            _prevPosition = Input.mousePosition;
         }
+        if(Input.GetMouseButton(0))
+        {
+            _differnce = _prevPosition - Input.mousePosition;
+            _prevPosition = Input.mousePosition;
+            if (_differnce == Vector3.zero)
+                return; 
+           if(Mathf.Abs(_differnce.y)>Mathf.Abs(_differnce.x))
+            {
+               if(_differnce.y < 0)
+                {
+                    _speed = Vector2.up;
+                }
+               else
+                {
+                    _speed = Vector2.up *-1;
+
+                }
+            }
+           //horizontal swipe
+           else
+            {
+                if (_differnce.x > 0)
+                {
+                    _speed = Vector2.up;
+                }
+                else
+                {
+                    _speed = Vector2.up * -1;
+                }
+            }
+            _transform.Rotate(_speed, Space.World);
+        }
+       
+
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    _transform.Rotate(Vector3.up,Space.World);
+        //}
     }
 }
